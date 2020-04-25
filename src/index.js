db2d910a0fe6cfcf18989e904db5225e862dcaa3
jucasoft <juca@jucasoft.com>
2020-04-25 20:06:50 +0200

    operatori custom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import {  } from 'rxjs'; 
+import {interval, Observable} from 'rxjs';
+// import {takeWhile} from "rxjs/operators";
+import {subLog} from "./utils";
+
+function takeWhile(fn) {
+    return source$ => new Observable(observer =>
+        source$.subscribe(
+            {
+                next: v => fn(v) ? observer.next() : observer.complete(),
+                error: e => observer.error(),
+                completion: value => fn(value) ? observer.next() : observer.complete()
+            }
+        )
+    )
+}
+
+interval(1000).pipe(
+    takeWhile(value => value < 2)
+).subscribe(subLog('test'))
