6c5ae86cd91e9eed886882a6fe42dd3ef09fae31
jucasoft <juca@jucasoft.com>
2020-04-25 15:14:09 +0200

    throttleTime

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import {  } from 'rxjs'; 
+import {interval} from 'rxjs';
+import {throttleTime} from "rxjs/operators";
+import {subLog} from "./utils";
+
+
+const source$ = interval(1000);
+source$.pipe(
+    throttleTime(2000)
+).subscribe(subLog('throttleTime'))
