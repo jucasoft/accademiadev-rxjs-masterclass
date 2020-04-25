e64b02fcdefe07676b4852f85320cf35805fc1b4
jucasoft <juca@jucasoft.com>
2020-04-25 15:22:07 +0200

    auditTime

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import {  } from 'rxjs'; 
+import {interval} from 'rxjs';
+import {auditTime, tap} from "rxjs/operators";
+import {subLog} from "./utils";
+
+const source$ = interval(1000);
+
+source$.pipe(
+    tap(interval => console.log('interval', interval)),
+    auditTime(2500)
+).subscribe(subLog('auditTime'))
