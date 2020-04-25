e9133ee9ef069ae9d0aa47474cab3383dc8cc7a3
jucasoft <juca@jucasoft.com>
2020-04-25 15:02:14 +0200

    Revert "partition"
    This reverts commit f19370d6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import {of, partition} from 'rxjs';
-import {filter} from "rxjs/operators";
-import {isOdd, subLog} from "./utils";
-
-const source$ = of(1, 2, 3, 4, 5, 6)
-
-// nel caso di filter, passano le emissioni che rispecchiano una determinata condizione, tutte le altre vengono perse.
-source$.pipe(
-    filter(isOdd)
-).subscribe(subLog('filter'))
-
-// utilizzando l'operatore partition, vengono creati due observable a particre da due condizioni.
-const [even$, odd$] = partition(source$, isOdd)
-
-even$.subscribe(subLog('even'));
-odd$.subscribe(subLog('odd'));
+import {  } from 'rxjs'; 
