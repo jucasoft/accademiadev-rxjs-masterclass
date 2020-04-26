0aa629064e096d1ba55481ee3f32744794fe2996
jucasoft <juca@jucasoft.com>
2020-04-27 00:53:51 +0200

    Subject vs AsyncSubject

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import {interval, ReplaySubject} from 'rxjs';
+import {AsyncSubject, interval, ReplaySubject} from 'rxjs';
 
-const source$ = new ReplaySubject(3);
+const source$ = new AsyncSubject(3);
 source$.next(1);
 source$.next(2);
 source$.next(3);
 source$.next(4);
 source$.next(5);
 source$.subscribe(console.log)
+source$.complete()
 
