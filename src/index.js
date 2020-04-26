f7fde1e963083dadcbd574d6b66b1c482ae17bc9
jucasoft <juca@jucasoft.com>
2020-04-27 01:02:25 +0200

    Rendere un Observable Hot con multicast e publish (publish)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import {AsyncSubject, interval, ReplaySubject, Subject} from 'rxjs';
-import {multicast} from "rxjs/operators";
+import {multicast, publish} from "rxjs/operators";
 
 const source$ = interval(500).pipe(
-    multicast(new Subject())
+    publish()
 );
 
 source$.connect();
