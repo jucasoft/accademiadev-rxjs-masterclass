6f1a67d483b957def3f1c6f9e235db900780d1ae
jucasoft <juca@jucasoft.com>
2020-04-27 01:17:52 +0200

    Risparmiare risorse con share e shareReplay

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import {AsyncSubject, from, interval, ReplaySubject, Subject} from 'rxjs';
-import {multicast, publish, refCount} from "rxjs/operators";
+import {interval, ReplaySubject, Subject} from 'rxjs';
+import {multicast, refCount, share, shareReplay, take} from "rxjs/operators";
+import {subLog} from "./utils";
 
-const source$ = from('loremipsum').pipe(
-    publish(),
-    refCount()
+const source$ = interval(500).pipe(
+    take(3),
+    // multicast(() => new Subject()),
+    // refCount()
+    share() // equivale a scrivere le righe precedenti.
 );
 
-setTimeout(()=>
-source$.subscribe(console.log)
-, 1000)
-setTimeout(()=>
-source$.subscribe(console.log)
-, 2000)
+setTimeout(() =>
+        source$.subscribe(subLog('source$ a').next)
+    , 1000)
+setTimeout(() =>
+        source$.subscribe(subLog('source$ b').next)
+    , 2000)
 
+const sourceB$ = interval(500).pipe(
+    take(3),
+    // multicast(() => new ReplaySubject()),
+    // refCount()
+    shareReplay() // equivale a scrivere le righe precedenti.
+);
+
+setTimeout(() =>
+        sourceB$.subscribe(subLog('sourceB a').next)
+    , 1000)
+setTimeout(() =>
+        sourceB$.subscribe(subLog('sourceB b').next)
+    , 2000)
