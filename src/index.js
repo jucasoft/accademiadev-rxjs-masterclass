3a0922202a246cb0206ad2f99c408190c6b5f29f
jucasoft <juca@jucasoft.com>
2020-04-27 16:04:42 +0200

    Risparmiare risorse con share e shareReplay

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import {  } from 'rxjs'; 
+import {BehaviorSubject, fromEvent, merge} from 'rxjs';
+import {mapTo} from "rxjs/operators";
+import {subLog} from "./utils";
+
+function createStore(initialState) {
+
+    const state$ = new BehaviorSubject(initialState);
+
+    function increment() {
+        state$.next(state$.getValue() + 1)
+    }
+
+    function decrement() {
+        state$.next(state$.getValue() - 1)
+    }
+
+    return {
+        getState: () => state$.asObservable(),
+        increment: () => increment(),
+        decrement: () => decrement(),
+    }
+}
+
+const store = createStore(0);
+store.getState().subscribe(subLog('store'));
+
+const sourceInc$ = fromEvent(document.querySelector('#increment'), 'click').pipe(mapTo(true));
+const sourceDec$ = fromEvent(document.querySelector('#decrement'), 'click').pipe(mapTo(false));
+
+merge(sourceInc$, sourceDec$).subscribe(next => {
+    if (next) {
+        store.increment();
+    } else {
+        store.decrement();
+    }
+})
