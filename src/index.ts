0e3c2f24d07676a7c8e1bcd1eba41fc622e0f8b4
jucasoft <juca@jucasoft.com>
2020-04-28 18:25:39 +0200

    Manipolare le sottoscrizioni con observeOn e subscribeOn

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 import './style.css'
+import {animationFrameScheduler, interval} from 'rxjs';
+import {observeOn, take} from 'rxjs/operators';
+
+
+const elem = <HTMLElement>document.querySelector('#unbelnome');
+
+
+// interval(1, animationFrameScheduler).pipe(
+//     take(300)
+// ).subscribe(
+//     function test(n) {
+//         elem.style.height = n + 'px';
+//     }
+// )
+
+interval(1).pipe(
+    take(300),
+    observeOn(animationFrameScheduler)
+).subscribe(n => elem.style.height = n + 'px')
+
