5e05a94399e5c79dc6aa2bab1c9f4426fb5af7cc
jucasoft <juca@jucasoft.com>
2020-04-29 18:08:27 +0200

    Manipolare le sottoscrizioni (animationFrameScheduler)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import './style.css'
 import {animationFrameScheduler, interval} from 'rxjs';
-import {observeOn, take} from 'rxjs/operators';
-
+import {map, observeOn, take} from 'rxjs/operators';
 
 const elem = <HTMLElement>document.querySelector('#unbelnome');
-
 
 // interval(1, animationFrameScheduler).pipe(
 //     take(300)
@@ -16,6 +14,7 @@ const elem = <HTMLElement>document.querySelector('#unbelnome');
 
 interval(1).pipe(
     take(300),
+    map(value => value),
     observeOn(animationFrameScheduler)
 ).subscribe(n => elem.style.height = n + 'px')
 
